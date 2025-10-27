a8d101f9bef3fa1429c91a881f314fb1827bc108
Farrel <farrelsheva13@gmail.com>
2025-10-27 18:39:48 +0700

    update: menambah font dan warna yang digunakan

---
+++ lib/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF0B0A0A); // dark grey coklat
+  static const Color gold = Color(0xFFFFD700);
+  static const Color goldDark = Color(0xFFF5C542);
+  static const Color textLight = Color(0xFFFFFFFF);
+  static const Color textMuted = Color(0xFFB0B0B0);
+  static const Color inputBorder = Color(0xFF8D8D8D);
+  static const Color white = Color(0xFFFFFFFF);
+}
