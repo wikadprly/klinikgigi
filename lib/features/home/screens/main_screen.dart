a6decc8a3fdd2a6457551575b72e0d2494742dcd
Faqih Ardiansyah <mofaqihardiansyah@gmail.com>
2025-11-02 00:03:32 +0700

    update: Memberi background dan warna gradient pada navbar

--- lib/features/home/screens/main_screen.dart
+++ lib/features/home/screens/main_screen.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_klinik_gigi/features/home/screens/home_screen.dart';
 import 'package:flutter_klinik_gigi/theme/colors.dart';
+import 'package:flutter_klinik_gigi/theme/text_styles.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-// --- PERBAIKI PATH IMPORT DI BAWAH INI ---
-// Asumsi file Pendaftaran ada di dalam folder 'reservasi_screens' (sesuaikan jika salah)
 import 'package:flutter_klinik_gigi/features/reservasi/screens/reservasi_screens.dart';
-// Nama foldernya 'dentalhome', bukan 'dental_home'
 import 'package:flutter_klinik_gigi/features/dentalhome/screens/dentalhome_screens.dart';
-// Nama foldernya 'riwayat_screens', bukan 'riwayat'
 import 'package:flutter_klinik_gigi/features/riwayat/screens/riwayat_screens.dart';
-// Nama foldernya 'profil_screens', bukan 'profil'
 import 'package:flutter_klinik_gigi/features/profil/screens/profil_screens.dart';
-// --- END PERBAIKAN PATH ---
+
+class GradientMask extends StatelessWidget {
+  const GradientMask({
+    required this.child,
+    required this.gradient,
+    this.blendMode = BlendMode.srcIn,
+    super.key,
+  });
+
+  final Widget child;
+  final Gradient gradient;
+  final BlendMode blendMode;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (bounds) => gradient.createShader(
+        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+      ),
+      blendMode: blendMode,
+      child: child,
+    );
+  }
+}
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -21,138 +40,198 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-// --- CLASS _MainScreenState YANG LENGKAP ---
 class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0; // Deklarasi _selectedIndex
+  int _selectedIndex = 0;
 
-  // Deklarasi _widgetOptions (Pastikan nama class-nya benar)
-  static const List<Widget> _widgetOptions = <Widget>[
-    HomeScreen(userId: '2'), // Ganti '1' dengan ID user valid Anda
-    PendaftaranScreen(), // Ganti dengan nama Class Pendaftaran/Reservasi Anda
-    DentalHomeScreen(), // Ganti dengan nama Class Dental Home Anda
-    RiwayatScreen(), // Ganti dengan nama Class Riwayat Anda
-    ProfilScreen(), // Ganti dengan nama Class Profil Anda
-  ];
+  late final List<Widget> _widgetOptions;
 
-  // Deklarasi _onItemTapped
+  @override
+  void initState() {
+    super.initState();
+    _widgetOptions = <Widget>[
+      HomeScreen(userId: '2', onNavigate: _onItemTapped),
+      PendaftaranScreen(),
+      DentalHomeScreen(),
+      RiwayatScreen(),
+      ProfilScreen(),
+    ];
+  }
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
-  // --- END CLASS DEFINITION ---
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ), // Sekarang dikenali
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/icons/home.svg', // Pastikan nama file SVG benar
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(
-                AppColors.textMuted,
-                BlendMode.srcIn,
+      body: Stack(
+        children: [
+          _widgetOptions.elementAt(_selectedIndex),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 90,
+              decoration: BoxDecoration(
+                color: AppColors.navbarBackground,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: BottomNavigationBar(
+                  items: <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                      icon: SvgPicture.asset(
+                        'assets/icons/home.svg',
+                        height: 24,
+                        width: 24,
+                        colorFilter: ColorFilter.mode(
+                          AppColors.textMuted,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                      activeIcon: GradientMask(
+                        gradient: AppColors.goldGradient,
+                        child: SvgPicture.asset(
+                          'assets/icons/home.svg',
+                          height: 24,
+                          width: 24,
+                          colorFilter: ColorFilter.mode(
+                            Colors.white,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                      label: 'Beranda',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: SvgPicture.asset(
+                        'assets/icons/reservasi_navbar.svg',
+                        height: 24,
+                        width: 24,
+                        colorFilter: ColorFilter.mode(
+                          AppColors.textMuted,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                      activeIcon: GradientMask(
+                        gradient: AppColors.goldGradient,
+                        child: SvgPicture.asset(
+                          'assets/icons/reservasi_navbar.svg',
+                          height: 24,
+                          width: 24,
+                          colorFilter: ColorFilter.mode(
+                            Colors.white,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                      label: 'Pendaftaran',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: SvgPicture.asset(
+                        'assets/icons/dentalhome_navbar.svg',
+                        height: 24,
+                        width: 24,
+                        colorFilter: ColorFilter.mode(
+                          AppColors.textMuted,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                      activeIcon: GradientMask(
+                        gradient: AppColors.goldGradient,
+                        child: SvgPicture.asset(
+                          'assets/icons/dentalhome_navbar.svg',
+                          height: 24,
+                          width: 24,
+                          colorFilter: ColorFilter.mode(
+                            Colors.white,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                      label: 'Dental Home',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: SvgPicture.asset(
+                        'assets/icons/riwayat_navbar.svg',
+                        height: 24,
+                        width: 24,
+                        colorFilter: ColorFilter.mode(
+                          AppColors.textMuted,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                      activeIcon: GradientMask(
+                        gradient: AppColors.goldGradient,
+                        child: SvgPicture.asset(
+                          'assets/icons/riwayat_navbar.svg',
+                          height: 24,
+                          width: 24,
+                          colorFilter: ColorFilter.mode(
+                            Colors.white,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                      label: 'Riwayat',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: SvgPicture.asset(
+                        'assets/icons/profil.svg',
+                        height: 24,
+                        width: 24,
+                        colorFilter: ColorFilter.mode(
+                          AppColors.textMuted,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                      activeIcon: GradientMask(
+                        gradient: AppColors.goldGradient,
+                        child: SvgPicture.asset(
+                          'assets/icons/profil.svg',
+                          height: 24,
+                          width: 24,
+                          colorFilter: ColorFilter.mode(
+                            Colors.white,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                      label: 'Profil',
+                    ),
+                  ],
+                  currentIndex: _selectedIndex,
+                  selectedItemColor: AppColors.gold,
+                  unselectedItemColor: AppColors.textMuted,
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  type: BottomNavigationBarType.fixed,
+                  showUnselectedLabels: true,
+                  onTap: _onItemTapped,
+                  selectedLabelStyle: AppTextStyles.label.copyWith(
+                    fontSize: 12,
+                  ),
+                  unselectedLabelStyle: AppTextStyles.label.copyWith(
+                    fontSize: 12,
+                  ),
+                ),
               ),
             ),
-            activeIcon: SvgPicture.asset(
-              'assets/icons/home.svg', // Pastikan nama file SVG benar
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(AppColors.gold, BlendMode.srcIn),
-            ),
-            label: 'Beranda',
-          ),
-          // ITEM KEDUA (PENDAFTARAN/RESERVASI)
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/icons/reservasi_navbar.svg', // Pastikan nama file SVG pendaftaran benar
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(
-                AppColors.textMuted,
-                BlendMode.srcIn,
-              ),
-            ),
-            activeIcon: SvgPicture.asset(
-              'assets/icons/reservasi_navbar.svg', // Pastikan nama file SVG pendaftaran benar
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(AppColors.gold, BlendMode.srcIn),
-            ),
-            label: 'Pendaftaran',
-          ),
-          // ITEM KETIGA (DENTAL HOME)
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/icons/dentalhome_navbar.svg', // Pastikan nama file SVG dental home benar
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(
-                AppColors.textMuted,
-                BlendMode.srcIn,
-              ),
-            ),
-            activeIcon: SvgPicture.asset(
-              'assets/icons/dentalhome_navbar.svg', // Pastikan nama file SVG dental home benar
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(AppColors.gold, BlendMode.srcIn),
-            ),
-            label: 'Dental Home',
-          ),
-          // ITEM KEEMPAT (RIWAYAT)
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/icons/riwayat_navbar.svg', // Pastikan nama file SVG riwayat benar
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(
-                AppColors.textMuted,
-                BlendMode.srcIn,
-              ),
-            ),
-            activeIcon: SvgPicture.asset(
-              'assets/icons/riwayat_navbar.svg', // Pastikan nama file SVG riwayat benar
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(AppColors.gold, BlendMode.srcIn),
-            ),
-            label: 'Riwayat',
-          ),
-          // ITEM KELIMA (PROFIL)
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/icons/profil.svg', // Pastikan nama file SVG profil benar
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(
-                AppColors.textMuted,
-                BlendMode.srcIn,
-              ),
-            ),
-            activeIcon: SvgPicture.asset(
-              'assets/icons/profil.svg', // Pastikan nama file SVG profil benar
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(AppColors.gold, BlendMode.srcIn),
-            ),
-            label: 'Profil',
           ),
         ],
-        currentIndex: _selectedIndex, // Sekarang dikenali
-        selectedItemColor: AppColors.gold,
-        unselectedItemColor: AppColors.textMuted,
-        backgroundColor: AppColors.background,
-        type: BottomNavigationBarType.fixed,
-        showUnselectedLabels: true,
-        onTap: _onItemTapped, // Sekarang dikenali
       ),
     );
   }
-} // <--- Kurung kurawal penutup untuk _MainScreenState
+}
