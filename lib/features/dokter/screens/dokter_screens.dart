02a52f6ea50eff5dbb16f07214841bb794912f7b
Faqih Ardiansyah <mofaqihardiansyah@gmail.com>
2025-11-02 00:05:17 +0700

    feat: Membuat screen untuk page jadwal praktek dan page dokter

---
+++ lib/features/dokter/screens/dokter_screens.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_klinik_gigi/theme/colors.dart';
+import 'package:flutter_klinik_gigi/theme/text_styles.dart';
+
+class DokterScreen extends StatelessWidget {
+  const DokterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        title: const Text('Dokter', style: AppTextStyles.heading),
+        backgroundColor: AppColors.background,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: AppColors.textLight),
+      ),
+      body: const Center(
+        child: Text('Ini Halaman Dokter', style: AppTextStyles.input),
+      ),
+    );
+  }
+}
