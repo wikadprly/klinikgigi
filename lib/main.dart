6db7a161bc6da1ec24cf33f46e4c74629a2df9e7
Farrel <farrelsheva13@gmail.com>
2025-11-02 20:38:18 +0700

    feat: menambah fitur shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
-// 🟢 main.dart (fix)
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart'; // ✅ penting untuk Provider
+import 'package:provider/provider.dart';
 import 'features/auth/screens/start.dart';
-import 'features/auth/providers/auth_provider.dart'; // ✅ pastikan path benar
-import 'features/auth/screens/daftar_pasien_lama.dart'; // ✅ untuk route tambahan nanti
-import 'features/auth/screens/daftar_pasien_baru.dart';
 import 'features/auth/screens/masuk.dart';
+import 'features/auth/screens/daftar_pasien_lama.dart';
+import 'features/auth/screens/daftar_pasien_baru.dart';
+import 'features/auth/providers/auth_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final authProvider = AuthProvider();
+  await authProvider.loadUser();
+
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
+      providers: [
+        ChangeNotifierProvider<AuthProvider>.value(value: authProvider),
+      ],
       child: const KlinikGigiApp(),
     ),
   );
@@ -21,6 +27,7 @@ class KlinikGigiApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Klinik Gigi',
@@ -28,16 +35,16 @@ class KlinikGigiApp extends StatelessWidget {
         brightness: Brightness.dark,
         primaryColor: const Color(0xFFFFD700),
         scaffoldBackgroundColor: const Color(0xFF0E0E10),
-        fontFamily: 'Poppins', // 🟢 diseragamkan kapital awal
+        fontFamily: 'Poppins',
       ),
-
-      // 🟢 perbaikan route name — Flutter tidak pakai ekstensi .dart
-      initialRoute: '/start',
+      initialRoute: authProvider.isLoggedIn ? '/dashboard' : '/start',
       routes: {
         '/start': (context) => const StartScreen(),
         '/masuk': (context) => const LoginPage(),
         '/daftar_pasien_lama': (context) => const DaftarPasienLamaPage(),
         '/daftar_pasien_baru': (context) => const DaftarPasienBaruPage(),
+        '/dashboard': (context) =>
+            const Placeholder(), //kel 7 udah bisa dbuat home yang nampilin nama pengguna karna sudah pakai shared preference
       },
     );
   }
