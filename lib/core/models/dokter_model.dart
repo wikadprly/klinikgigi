eaad98218533e525a4e013eaf9b2a501c006acab
Faqih Ardiansyah <mofaqihardiansyah@gmail.com>
2025-11-11 15:49:36 +0700

    update: Memperbarui logika dari model dokter

--- lib/core/models/dokter_model.dart
+++ lib/core/models/dokter_model.dart
@@ -13,9 +13,21 @@ class DokterModel {
 
   factory DokterModel.fromJson(Map<String, dynamic> json) {
     return DokterModel(
+<<<<<<< HEAD
       dokterId: json['dokter_id'],
       namaDokter: json['nama_dokter'],
       spesialisasi: json['spesialisasi'],
+=======
+      dokterId: json['dokter_id'].toString(),
+
+      // Jika 'nama_dokter' null, gunakan string kosong ''
+      namaDokter: json['nama_dokter'] ?? '',
+
+      // Jika 'spesialisasi' null, gunakan string kosong ''
+      spesialisasi: json['spesialisasi'] ?? '',
+
+      // fotoProfil sudah aman karena tipenya String? (nullable)
+>>>>>>> cb9a1c9 (update: Memperbarui logika dari model dokter)
       fotoProfil: json['foto_profil'],
     );
   }
