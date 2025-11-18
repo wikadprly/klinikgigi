878ac811c5fa2e539150ab5d54d0e6ef55601bbe
wikadprly <wikkadwia@gmail.com>
2025-11-18 09:11:01 +0700

    feat: Memperbarui models untuk reservasi dan membuat widget reservasi

--- lib/core/models/master_jadwal_model.dart
+++ lib/core/models/master_jadwal_model.dart
@@ -1,53 +1,45 @@
 // lib/core/models/master_jadwal_model.dart
 
 class MasterJadwalModel {
-  final int id;
+  final int jadwalId;
   final String kodeDokter;
+  final String namaDokter;
   final String kodePoli;
-  final String hari;
+  final String namaPoli;
+  final String hari;         // ⬅⬅⬅ TAMBAHKAN INI
   final String jamMulai;
   final String jamSelesai;
-  final String keterangan;
-  final int quota;
+  final int kuotaTotal;
+  final int kuotaTerpakai;
+  final int sisaKuota;
 
   MasterJadwalModel({
-    required this.id,
+    required this.jadwalId,
     required this.kodeDokter,
+    required this.namaDokter,
     required this.kodePoli,
-    required this.hari,
+    required this.namaPoli,
+    required this.hari,        // ⬅⬅ TAMBAHKAN INI
     required this.jamMulai,
     required this.jamSelesai,
-    required this.keterangan,
-    required this.quota,
+    required this.kuotaTotal,
+    required this.kuotaTerpakai,
+    required this.sisaKuota,
   });
 
   factory MasterJadwalModel.fromJson(Map<String, dynamic> json) {
     return MasterJadwalModel(
-      id: json['id'] is String
-          ? int.tryParse(json['id']) ?? 0
-          : json['id'] ?? 0,
+      jadwalId: json['jadwal_id'] ?? 0,
       kodeDokter: json['kode_dokter'] ?? '',
+      namaDokter: json['nama_dokter'] ?? '',
       kodePoli: json['kode_poli'] ?? '',
-      hari: json['hari'] ?? '',
+      namaPoli: json['nama_poli'] ?? '',
+      hari: json['hari'] ?? '-',                 // ⬅⬅⬅ BACA DARI API
       jamMulai: json['jam_mulai'] ?? '',
       jamSelesai: json['jam_selesai'] ?? '',
-      keterangan: json['keterangan'] ?? '',
-      quota: json['quota'] is String
-          ? int.tryParse(json['quota']) ?? 0
-          : json['quota'] ?? 0,
+      kuotaTotal: json['kuota_total'] ?? 0,
+      kuotaTerpakai: json['kuota_terpakai'] ?? 0,
+      sisaKuota: json['sisa_kuota'] ?? 0,
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'kode_dokter': kodeDokter,
-      'kode_poli': kodePoli,
-      'hari': hari,
-      'jam_mulai': jamMulai,
-      'jam_selesai': jamSelesai,
-      'keterangan': keterangan,
-      'quota': quota,
-    };
   }
 }
