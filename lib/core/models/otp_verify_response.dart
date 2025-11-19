a3dbee287154f471cf82ceada450dbb58bf3e6e3
rafli <raflyfirmansyah987@gmail.com>
2025-11-19 10:32:52 +0700

    Update: Memperbaiki tabrakan otp respon

--- lib/core/models/otp_verify_response.dart
+++ lib/core/models/otp_verify_response.dart
@@ -1,31 +1,22 @@
 class OtpVerifyResponse {
   final bool success;
   final String message;
-<<<<<<< HEAD
   final Map<String, dynamic>? data;
+  final String? resetToken;
 
-  OtpVerifyResponse({required this.success, required this.message, this.data});
+  OtpVerifyResponse({
+    required this.success,
+    required this.message,
+    this.data,
+    this.resetToken,
+  });
 
   factory OtpVerifyResponse.fromJson(Map<String, dynamic> json) {
     return OtpVerifyResponse(
       success: json['success'] ?? false,
       message: json['message'] ?? '',
       data: json['data'],
-=======
-  final String? resetToken;
-
-  OtpVerifyResponse({
-    required this.success,
-    required this.message,
-    this.resetToken,
-  });
-
-  factory OtpVerifyResponse.fromJson(Map<String, dynamic> json) {
-    return OtpVerifyResponse(
-      success: json['success'],
-      message: json['message'],
       resetToken: json['reset_token'],
->>>>>>> Jeffrey
     );
   }
 }
